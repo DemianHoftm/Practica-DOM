e057c49b780cf6c732b5ad8e7b842940bfcba6a4
DemianHoftm <Thegodneko@gmail.com>
2024-10-25 16:26:35 -0400

    part 2

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
+let pressed = false
 window.onload = function() {
     const button = document.getElementById("ShowWife");
     button.addEventListener("click", ShowButtonPressed);
 
     function ShowButtonPressed() {
-        const image = document.createElement("img")
-        image.src = "./assests/sop2.jpg"
-        image.alt = "She's my woman (She's a robot)";
-        image.className = "img";
-        document.body.appendChild(image)
-        document.body.innerHTML = "";
+        console.log(pressed)
+        if(pressed){
+            location.reload();
+        }else{
+            const title = document.getElementById("PageTitle")
+            title.textContent= "Look? She's so beautiful bro"
+            const Subtitle = document.getElementById("TextFromBody")
+            Subtitle.textContent= "She's the fear of sangvis ferri and a really cute girl. Also quite violent"
+            const image = document.createElement("img")
+            image.src = "./assests/sop2.jpg"
+            image.alt = "She's my woman (She's a robot)";
+            image.className = "img";
+            document.body.appendChild(image)
+            button.textContent= "go back"
+            pressed = true;
+        }
+        
     }
 };
